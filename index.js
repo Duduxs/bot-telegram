bddd67e9aa7164a6dd9efe5100a5b8405887cb17
Duduxs <duduxss3@gmail.com>
2020-07-26 18:25:09 -0300

    Removed my password from Google and Telegram

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+//Require the libs
 const Telegraf = require('telegraf');
+const dialogflow = require('./dialogflow');
+
+//Only dev
+const config = require('./config.json');
+
 //Bot Object with my token.
-const bot = new Telegraf('1313808778:AAGXdHfsYoQ2pNZ-2rwNfDVPuxTLOmxd5S0');
-//What my bot will do when the user put the /start in the conversation
-bot.start((ctx) =>{
-  ctx.reply('Test');
+const bot = new Telegraf(config.token);
+
+
+
+//What my bot will do when the user put a message in the conversation
+bot.on('message', async function (ctx) {
+  const chatId = ctx.message.chat.id;
+
+  const dfResponse = await dialogflow.sendMessage(chatId.toString(), ctx.message.text);
+
+  ctx.reply(dfResponse.text);
 });
+
 //Initialize the bot.
 bot.launch();
